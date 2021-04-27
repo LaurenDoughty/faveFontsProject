c77828fac787b33a9942c3488558c8f97a51d09b
Lauren <lefdoughty@gmail.com>
2021-04-27 11:47:23 -0400

    updated index, css & js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,65 @@
-var phraseLength = 0;
-var phrase = 'Type something.';
 
-function myFunction() {
-  document.getElementById("boxShadow").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+var typeText = document.querySelector(".typeText")
+var textToBeTyped = "Type something."
+var index = 0, isAdding = true
+
+
+var nameField = document.getElementById('nameField');
+var lastNameValue = undefined;
+
+updateNameDisplay();
+
+setInterval(updateNameDisplay, 100);
+
+function updateNameDisplay() {
+  var thisValue = nameField.value || "Call me Ishmael.";
+  if (lastNameValue != thisValue) {
+    document.getElementById('nameDisplay').innerHTML = lastNameValue = thisValue;
+  }
 }
+
+/*
+var elems = document.getElementsByClassName('cardText');
+for(var i = 0; i < elems.length; i++) {
+    elems[i].textContent = 'This should change';
+  }
+}
+*/
+
+function playAnim() {
+  setTimeout(function () {
+    // set the text of typeText to a substring of
+    // the textToBeTyped using index.
+    typeText.innerText = textToBeTyped.slice(0, index)
+    if (isAdding) {
+      // adding text
+      if (index > textToBeTyped.length) {
+        // no more text to add
+        isAdding = false
+        //break: wait 4s before playing again
+        setTimeout( function () {
+          playAnim()
+        }, 4000)
+        return
+      } else {
+        // increment index by 1
+        index++
+      }
+    } else {
+      // removing text
+      if (index === 0) {
+        // no more text to remove
+        isAdding = true
+      } else {
+        // decrement index by 1
+        index--
+      }
+    }
+    // call itself
+    playAnim()
+  }, 120)
+}
+// start animation
+playAnim()
+
+
